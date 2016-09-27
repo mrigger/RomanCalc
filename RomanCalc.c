18c5649fcd8e634e88d31fc8c9c8f1be865aa9b7
DanielFelixKim <daniel.felixkim@gmail.com>
2016-09-27 10:26:06 -0500

    Add files via upload

--- RomanCalc.c
+++ RomanCalc.c
@@ -10,13 +10,18 @@ Roman Numeral Calculator
 
 void RomAdd (char*); // Function to add and simplify two Roman numerals
 void Addexpand (char*); // Function to expand Roman numerals and concatenate 
+void RomSubtract (char*); // Function to subtract and simplify two Roman numerals
 int counter(char*); // Function to count each appearance of a Roman numeral
-int secondcounter(char*); // Function to count each appearance of a Roman numeral in second number during subtraction
+int subcountera(char*); // Function to count each appearance of a Roman numeral
+int subcounterb(char*); // Function to count each appearance of a Roman numeral in second number during subtraction
 int count[7] = {0, 0, 0, 0, 0, 0, 0}; // Counter to keep track of Roman Numerals in order from M to I
-int secondcount[7] = {0, 0, 0, 0, 0, 0, 0}; // Secon counter for subtraction to keep track of Roman Numerals in order from M to I
+int holdcount[7] = {0, 0, 0, 0, 0, 0, 0}; // Counter to keep track of Roman Numerals in order from M to I
+int subcounta[7] = {0, 0, 0, 0, 0, 0, 0}; // Counter for subtraction to keep track of Roman Numerals before - sign
+int subcountb[7] = {0, 0, 0, 0, 0, 0, 0}; // Second counter for subtraction to keep track of Roman after - sign
 int opcheck (char*);   	// Check if adding (1) or subtracting (0)
 int operationcheck; // =1 if adding or =0 if subtracting
-void simplify (char*); // Function to finalize Roman Numerals
+void simplifya (char*); // Function to finalize Roman Numerals
+void simplifyb (char*);
 void Subtexpand (char*); // Function to expand Roman numerals and keep separate 
 
 
@@ -31,14 +36,31 @@ int main(void)
 	if (operationcheck == 1) // Adding
 	{
 		Addexpand(input); // Expand the Roman Numerals
-		RomAdd(input); // Add them into one numeral
-		simplify(input); // Simplify 
+		simplifya(input); // Add them into one numeral
+		simplifyb(input); // Simplify 
 		printf("%s\n", input);  // Print
 	}
 	else if (operationcheck == 0)
 	{
 		Subtexpand(input);
+		subcountera(input);
+		for (int a = 0; a < 7; a++)
+			{
+				printf("a: %d\n", subcounta[a]);
+			}
+		subcounterb(input);
+		for (int q = 0; q < 7; q++)
+			{
+				printf("b: %d\n", subcountb[q]);
+			}
+		RomSubtract(input);
+		simplifyb(input);
+		for (int q = 0; q < 7; q++)
+			{
+				printf("LASTCOUNT: %d\n", count[q]);
+			}
 		printf("%s\n", input);
+		
 	}
 	else if (operationcheck == 2) 
 	{
@@ -49,7 +71,7 @@ int main(void)
 }
 
 // ADD Functions
-void RomAdd (char* r)
+void simplifya (char* r)
 {
 	int i = 0,j = 0, limit = 0; 
 	char *pointer, *result;		// pointer: one for the current string being looked at, and one for the outputted string
@@ -64,12 +86,15 @@ void RomAdd (char* r)
 	index[6] = "I";
 	result = (char*)malloc(length+1);
 	
-	for (int v =0; v < 7; v++) // Check Counter
+	if (operationcheck == 1)
 	{
-		count[v] = 0;
+		for (int v =0; v < 7; v++) // Check Counter
+		{
+			count[v] = 0;
+		}
+		counter(r);	
 	}
-	counter(r);	
-
+	
 	for (i = 6; i >= 0; i--) 
 	{
 		if ((count[i] >= 2) && ((i == 5) || (i == 3) || (i == 1))) // V, L, D can only appear once
@@ -90,19 +115,164 @@ void RomAdd (char* r)
 		}
 	}
 	
+	for (int v =0; v < 7; v++) 
+	{
+		holdcount[v] = count[v];
+	}
+
 	for (i = 0; i < 7; i++)  // Write information from counter to create a Roman Numeral String
 	{
-		while (count [i] > 0) 
+		while (holdcount [i] > 0) 
 		{
 			*(result+j) = *index[i];
 			j++;
-			count[i]--;
+			holdcount[i]--;
 		}
 	}
 	pointer = r;
 	*(result+j) = '\0';
 	strcpy(r, result);
 	free(result);	
+}
+
+//Simplify
+
+void simplifyb(char *q)
+{
+	int i = 0,j = 0, limit = 0; 
+	char *pointer, *result;
+	int length = strlen(q);
+	const char *index[7];
+	index[0] = "M";
+	index[1] = "D";
+	index[2] = "C";
+	index[3] = "L";
+	index[4] = "X";
+	index[5] = "V";
+	index[6] = "I";
+	result = (char*)malloc(length+1);
+	
+	for (i = 0; i < 7; i++)
+	{
+		while (count [i] > 0)
+		{
+		
+			if ((count[i] == 4) && ((i == 6) || (i == 4) || (i == 2)) && (count[i-1] == 0)) // Ex: IIII -> IV
+			{
+				*(result+j) = *index[i];
+				j++;
+				*(result+j) = *index[i-1];
+				j++;
+				count[i] = 0;
+			}
+			else if ((count[i+1] == 4) && ((i == 5) || (i == 3) || (i == 1)) && (count[i] == 1)) // Ex: VIIII -> IX
+			{
+				*(result+j) = *index[i+1];
+				j++;
+				*(result+j) = *index[i-1];
+				j++;
+				count[i] = 0;
+				count[i+1] = 0;
+			}
+			else
+			{
+				*(result+j) = *index[i];
+				j++;
+				count[i]--;
+			}
+		}	
+	}
+	pointer = q;
+	*(result+j) = '\0';
+	strcpy(q, result);
+	free(result);	
+}
+
+void RomSubtract(char *b)
+{
+	int i = 0, j = 0, k = 0, d = 1, borrow = 0; 
+	char *pointer, *result;		// pointer: one for the current string being looked at, and one for the outputted string
+	int length = strlen(b);
+	const char *index[7]; // index used to create string
+	index[0] = "M";
+	index[1] = "D";
+	index[2] = "C";
+	index[3] = "L";
+	index[4] = "X";
+	index[5] = "V";
+	index[6] = "I";
+	result = (char*)malloc(length+1);
+	
+	for (int v =0; v < 7; v++) // Check Counter
+	{
+		subcounta[v] = 0;
+	}
+	subcountera(b);
+	for (int v =0; v < 7; v++) // Check Counter
+	{
+		subcountb[v] = 0;
+	}
+	subcounterb(b);
+	
+	for (int v =0; v < 7; v++) // Subtract counters
+	{
+		count[v] = subcounta[v] - subcountb[v];
+	}
+	
+			
+	for (k = 6; k >= 0; k--) 
+	{
+		if ((count[k] < 0) && (count[k-1] <= 0))
+		{
+			printf("k: %d\n",k);
+			while (count[k-d] <= 0 )
+			{
+				d++; 
+			}
+			for (int r = d; r >= 0; r--)
+			{
+				if (count[k] < 0)
+				{
+					printf("lol\n");
+					if ((r == 0) || (r == 2) || (r == 4))	
+					{
+						borrow = 2;
+					}
+					if ((r == 1) || (r == 3) || (r == 5))	
+					{
+						borrow = 5;
+					}
+					count[k-r]--;
+					count[k-r+1] = count[k-r+1] + borrow;
+				}
+				printf("R: %d\n", r);		
+			}
+			
+		}
+		
+		else
+		{
+			for (int r = 0; r < d; r++)
+			{
+				if (count[k] < 0)
+				{
+					printf("YO k: %d\n",k);
+					count[k-1]--;
+					if ((k == 0) || (k == 2) || (k == 4) || (k==6))	
+					{
+						borrow = 5;
+					}
+					if ((k == 1) || (k == 3) || (k == 5))	
+					{
+						borrow = 2;
+					}
+					count[k] = count[k] + borrow;	
+				}	
+			}
+		}
+	d = 1;		
+	}
+
 }
 
 
@@ -200,7 +370,6 @@ void Addexpand(char *s) // Expand function: Scans Roman Numeral equation and con
 	free(result);
 }
 
-
 //Counter Functions
 int counter(char *u) // Counts number of individual characters
 {	
@@ -234,7 +403,7 @@ int counter(char *u) // Counts number of individual characters
 	return count[7];
 }
 
-int secondcounter(char *f) // Would count number of individual characters after - sign
+int subcountera(char *f) // Counts number of individual characters before - sign
 {	
 	int i = 0, j = 0, k = 0;
 	char *pointer, *result;
@@ -246,25 +415,79 @@ int secondcounter(char *f) // Would count number of individual characters after 
 	index[4] = "X";
 	index[5] = "V";
 	index[6] = "I";
+	const char *minus = "-";
 	int length = strlen(f);
+	int newlength = 0;
 	
 	result = (char*)malloc(length+1);
 	
 	pointer = f;
 	
+	for (k = 0; k < length; k++)
+	{
+		if (pointer[k] == *minus)
+		{
+			k = length;
+		}
+		newlength++;
+	}
+	
 	for (j = 0; j <7; j++)
 	{	
-		for (i = 0;  i < (length) ; i++)
+		for (i = 0;  i < newlength ; i++)
 		{
 			if (pointer[i] == *index[j]) 
 			{
-				(secondcount[j])++;
-				
+				(subcounta[j])++;
 			}
 		}
 	}
-	return secondcount[7];
+	return subcounta[7];
 }
+
+
+int subcounterb(char *f) // Counts number of individual characters before - sign
+{	
+	int i = 0, j = 0, k = 0;
+	char *pointer, *result;
+	const char *index[7];
+	index[0] = "M";
+	index[1] = "D";
+	index[2] = "C";
+	index[3] = "L";
+	index[4] = "X";
+	index[5] = "V";
+	index[6] = "I";
+	const char *minus = "-";
+	int length = strlen(f);
+	int newlength = 0;
+	
+	result = (char*)malloc(length+1);
+	
+	pointer = f;
+	
+	for (k = 0; k < length; k++)
+	{
+		if (pointer[k] == *minus)
+		{	
+			for (j = 0; j <7; j++)
+			{	
+				for (i = k;  i < (length) ; i++)
+				{
+					if (pointer[i] == *index[j]) 
+					{
+						(subcountb[j])++;	
+					}
+				}
+			}
+		}
+	}
+
+	return subcountb[7];
+}
+
+
+
 
 
 
@@ -304,67 +527,9 @@ int opcheck (char*t)
 	return operationcheck;
 }
 
-//Simplify
-
-void simplify(char *q)
-{
-	int i = 0,j = 0, limit = 0; 
-	char *pointer, *result;
-	int length = strlen(q);
-	const char *index[7];
-	index[0] = "M";
-	index[1] = "D";
-	index[2] = "C";
-	index[3] = "L";
-	index[4] = "X";
-	index[5] = "V";
-	index[6] = "I";
-	result = (char*)malloc(length+1);
-	
-	for (int v =0; v < 7; v++) // Get count
-	{
-		count[v] = 0;
-	}
-	counter(q);	
-
-	for (i = 0; i < 7; i++)
-	{
-		while (count [i] > 0)
-		{
-		
-			if ((count[i] == 4) && ((i == 6) || (i == 4) || (i == 2)) && (count[i-1] == 0)) // Ex: IIII -> IV
-			{
-				*(result+j) = *index[i];
-				j++;
-				*(result+j) = *index[i-1];
-				j++;
-				count[i] = 0;
-			}
-			else if ((count[i+1] == 4) && ((i == 5) || (i == 3) || (i == 1)) && (count[i] == 1)) // Ex: VIIII -> IX
-			{
-				*(result+j) = *index[i+1];
-				j++;
-				*(result+j) = *index[i-1];
-				j++;
-				count[i] = 0;
-				count[i+1] = 0;
-			}
-			else
-			{
-				*(result+j) = *index[i];
-				j++;
-				count[i]--;
-			}
-		}	
-	}
-	pointer = q;
-	*(result+j) = '\0';
-	strcpy(q, result);
-	free(result);	
-}
 
 
-// Expand
+// Expand for subtraction
 void Subtexpand(char *a) // Expand function: 
 {	
 	int i = 0, j = 0, length; // used for counters
